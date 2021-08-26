ff5f0accc2690c65a2a47b6114a2eb3b29e8c64e
LIAN <lian@LIANdeMacBook-Air.local>
2021-08-26 18:18:01 +0800

    配置中心创建

---
+++ eureka/src/main/java/cn/github/zeroclian/EurekaApplication.java
@@ -0,0 +1,18 @@
+package cn.github.zeroclian;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: qiyiguo
+ * @Date: 2021-08-26 3:04 下午
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
