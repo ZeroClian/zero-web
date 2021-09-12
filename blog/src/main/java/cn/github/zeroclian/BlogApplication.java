ca5c5d8437d644fd1edd67e5c73e4af39b515e19
ZeroClian <zeroycl@163.com>
2021-09-12 18:42:44 +0800

    update：注册中心更换为nacos

--- blog/src/main/java/cn/github/zeroclian/BlogApplication.java
+++ blog/src/main/java/cn/github/zeroclian/BlogApplication.java
@@ -2,14 +2,14 @@ package cn.github.zeroclian;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Author: qiyiguo
  * @Date: 2021-09-02 10:49 上午
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class BlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class, args);
