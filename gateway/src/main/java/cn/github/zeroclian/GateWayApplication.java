7f091afb237d6df3103bcb8b947b3cde639bd81e
ZeroClian <zeroycl@163.com>
2021-09-13 14:28:28 +0800

    add：gateway网关模块

---
+++ gateway/src/main/java/cn/github/zeroclian/GateWayApplication.java
@@ -0,0 +1,18 @@
+package cn.github.zeroclian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Desciption
+ * @Author: qiyiguo
+ * @Date: 2021-09-13 11:20 上午
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
